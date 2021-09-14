06cf01151d008313c8a16686e0e56652ec17409e
lkMatheus <90664684+lkMatheus@users.noreply.github.com>
2021-09-13 23:17:13 -0300

    Add files via upload

---
+++ ReinoAnimal/src/Animal.java
@@ -0,0 +1,17 @@
+
+
+public class Animal {
+
+	String Classe;
+	String Animais;
+	String Tipo;
+	
+	void status() {
+		System.out.print("O Animal"+"  "+this.Animais +"   " + this.Classe +"    "+ this.Tipo + "\n");
+	}
+	
+	
+	
+	
+	
+}
---
+++ ReinoAnimal/src/Reino.java
@@ -0,0 +1,24 @@
+
+public class Reino {
+
+	public static void main(String[] args) {
+	Animal a1 = new Animal();
+	a1.Animais = "Girafa";
+	a1.Classe = "Terrestre";
+	a1.Tipo = "Mamifero";
+	a1.status();
+	
+	Animal a2 = new Animal();
+	a2.Animais = "Morcego";
+	a2.Classe = "Voador";
+	a2.Tipo = "Mamifero";
+	a2.status();
+	
+	Animal a3 = new Animal();
+	a3.Animais = "Baleia Azul";
+	a3.Classe = "Aquatico";
+	a3.Tipo = "Mamifero";
+	a3.status();
+	}
+
+}
